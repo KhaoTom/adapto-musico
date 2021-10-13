8ee31163aef9c134f01fd1c0f99bc790754958da
Edwin DeNicholas <disruption999@hotmail.com>
2021-10-12 19:59:05 -0700

    Add area.1 being tree trunk.

--- Assets/Scripts/RotateTransform.cs
+++ Assets/Scripts/RotateTransform.cs
@@ -6,9 +6,18 @@ public class RotateTransform : MonoBehaviour
 {
     public Vector3 axis = Vector3.up;
     public float speed = 10;
+    public float delta = 0;
+    public float minSpeed = 0;
+    public float maxSpeed = 50;
 
     void Update()
     {
         transform.Rotate(axis, speed * Time.deltaTime);
+        speed = Mathf.Clamp(speed + delta * Time.deltaTime, minSpeed, maxSpeed);
+    }
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
     }
 }
